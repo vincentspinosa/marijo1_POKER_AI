22218fbc7e986c2e6d5b55181d752ce8c1cb0fc3
Vincent Spinosa <vincentspinosa@gmail.com>
2023-05-11 13:42:52 +0200

    probleme avec fold/check réglé

--- cfr.py
+++ cfr.py
@@ -15,44 +15,36 @@ def compute_probabilities(array):
 def cfr(gameState, seconds):
     liste_actions = gameState.available_actions()
     probabilities = [[el, 0] for el in liste_actions]
+    opposite_player_index = (gameState.get_player_position(gameState.target_player) + 1) % len(gameState.players)
     gameStateInitial = copy.deepcopy(gameState)
-    gameStateTemp = copy.deepcopy(gameState)
     start_time = time.time()
     iterations = 0
     while (time.time() - start_time) < seconds:
+        gameStateTemp = copy.deepcopy(gameStateInitial)
         maxReward = None
         index = -1
         for action in liste_actions:
-            opposite_player_index = (gameStateTemp.get_player_position(gameStateTemp.target_player) + 1) % len(gameStateTemp.players)
+            gameStateTemp.deck.shuffle()
             reward = 0
-            gameStateTemp.handle_action(action[0], raise_amount=action[1])
+            sumAction = gameStateTemp.current_pot + action[1]
             if action[0] == 'fold':
-                reward = 0 - gameStateTemp.current_pot
+                reward -= sumAction
             else:
                 gameStateTemp.go_to_showdown()
                 gameStateTemp.players[opposite_player_index].hand = [gameStateTemp.deck.deal() for _ in range(2)]
                 winner = gameStateTemp.showdown(gameStateTemp.players)
                 if winner == gameStateTemp.target_player:
-                    reward = gameStateTemp.current_pot
+                    reward += sumAction
                 elif winner == gameStateTemp.players[opposite_player_index]:
-                    reward == 0 - gameStateTemp.current_pot
-            gameStateTemp = copy.deepcopy(gameStateInitial)
-            gameStateTemp.deck.shuffle()
+                    reward -= sumAction
             if maxReward is not None:
-                if reward < maxReward:
+                if reward < maxReward or (reward == maxReward and random.choice([True, False])):
                     break
-                elif reward == maxReward:
-                    if random.choice([True, False]):
-                        break
-            maxReward = reward
+            maxReward = copy.copy(reward)
             index += 1
             iterations += 1
         probabilities[index][1] += 1
     print(f"\nNUMBER OF ITERATIONS: {iterations}")
-    sum = 0
-    for i in range(1, len(probabilities)):
-        sum += probabilities[i][1]
-    probabilities[0][1] = iterations - sum
     print("Probabilities:")
     for p in probabilities:
         print(p)
