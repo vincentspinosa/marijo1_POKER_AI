9a8c88b09282a621f8b94ce84b580644f82752ae
Vincent Spinosa <vincentspinosa@gmail.com>
2023-05-12 14:16:03 +0200

    ui améliorée

--- ui.py
+++ ui.py
@@ -1,19 +1,25 @@
 from gameState import GameState
 from player import Player
 
-n_players = 2
 sm_bld = int(input("Small blind: "))
 bg_bld = int(input("Blig blind: "))
-players = ()
-for i in range(n_players):
-    players += (Player(chips=int(input(f"Player {i} chips: "))), )
-target_player_index = int(input("Target player index: "))
-game_state = GameState(players, target_player_index, small_blind=sm_bld, big_blind=bg_bld)
-game_state.play_preflop()
-if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
-    game_state.play_flop()
-if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
-    game_state.play_turn()
-if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
-    game_state.play_river()
-print("Hand done.\n")
+while True:
+    n_players = 2
+    players = ()
+    target_player_chips = int(input("Target player chips: "))
+    target_player_index = int(input("Target player index: "))
+    if target_player_index == 0:
+        players += (Player(chips=target_player_chips), )
+        players += (Player(chips=int(input("Opposite player chips: "))), )
+    else:
+        players += (Player(chips=int(input("Opposite player chips: "))), )
+        players += (Player(chips=target_player_chips), )
+    game_state = GameState(players, target_player_index, small_blind=sm_bld, big_blind=bg_bld)
+    game_state.play_preflop()
+    if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
+        game_state.play_flop()
+    if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
+        game_state.play_turn()
+    if len(game_state.active_players) > 1 and len(game_state.all_in_players) + 1 < len(game_state.active_players):
+        game_state.play_river()
+    print("Hand done.\n")
