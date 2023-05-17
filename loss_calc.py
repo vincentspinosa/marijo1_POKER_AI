2cfee5ce3d8d5459bf28f68732cacd95410ed29c
Vincent Spinosa <vincentspinosa@gmail.com>
2023-05-17 18:09:37 +0200

    plot in terminal for loss_calc

--- loss_calc.py
+++ loss_calc.py
@@ -1,19 +1,21 @@
 import copy
 from player import Player
 from gameState import GameState
+import plotext as plt
 from cfr import cfr
 
 p1 = Player(chips=1000)
 p2 = Player(chips=1000)
 game_state = GameState((p1, p2), 1, small_blind=10, big_blind=20)
 
-start = 51
-iterations = 10
-for second in range(start * -1, 0, 5):
-    print(f"\n{second * -1} seconds:")
+start = 10
+iterations = 50
+plot = [[], []]
+for second in range(start, 0, -1):
+    print(f"\n{second} seconds:")
     loss = []
     for iter in range(iterations):
-        cfr_result = cfr(copy.deepcopy(game_state), second * -1)
+        cfr_result = cfr(copy.deepcopy(game_state), second)
         print(f"\nIteration {iter}")
         if iter == 0:
             for action in cfr_result[0]:
@@ -26,8 +28,16 @@ for second in range(start * -1, 0, 5):
                     loss[i][1][0] = copy.copy(cfr_result[0][i][1])
                 elif loss[i][1][1] < cfr_result[0][i][1]:
                     loss[i][1][1] = copy.copy(cfr_result[0][i][1])
-    totalLoss= 0
+    totalLoss = 0
     for el in loss:
         print(el)
         totalLoss += el[1][1] - el[1][0]
     print(f"\nTotal loss: {int(totalLoss * 10000) / 100}".upper())
+    plot[0].append(second)
+    plot[1].append(totalLoss)
+for el in plot[0]:
+    print(el)
+for el in plot[1]:
+    print(el)
+plt.bar(plot[0], plot[1])
+plt.show()
