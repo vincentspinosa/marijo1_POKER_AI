3db0e75d45802c958238a458590c10ca88d8befa
Vincent Spinosa <vincentspinosa@gmail.com>
2023-05-21 11:56:39 +0200

    function 'ask_card_input()' created

--- ui/ui.py
+++ ui/ui.py
@@ -121,30 +121,30 @@ class UI(gameState.GameState):
         else:
             self.determine_community_cards()
         self.play_round()
-    
-    def determine_hole_cards(self):
-        for x in range(2):
-            index = -1
-            for i in self.deck.cards:
-                index += 1
-                print(f"{index} - {i}")
-            y = helpers.force_gui_int_input(f"\nCard {x}: ")
-            self.target_player.hand.append(self.deck.cards[y])
-            self.deck.cards.pop(y)
 
-    def determine_community_cards(self):
-        if self.current_stage == 'flop':
-            nb_cards = 3
-        elif self.current_stage in ['turn', 'river']:
-            nb_cards = 1
+    def ask_card_input(self, nb_cards, hole_cards=True):
         for x in range(nb_cards):
             index = -1
             for i in self.deck.cards:
                 index += 1
                 print(f"{index} - {i}")
             y = helpers.force_gui_int_input(f"\nCard {x}: ")
-            self.community_cards.append(self.deck.cards[y])
+            if hole_cards == True:
+                self.target_player.hand.append(self.deck.cards[y])
+            else:
+                self.community_cards.append(self.deck.cards[y])
             self.deck.cards.pop(y)
+
+    
+    def determine_hole_cards(self):
+        self.ask_card_input(2, hole_cards=True)
+
+    def determine_community_cards(self):
+        if self.current_stage == 'flop':
+            nb_cards = 3
+        elif self.current_stage in ['turn', 'river']:
+            nb_cards = 1
+        self.ask_card_input(nb_cards, hole_cards=False)
 
     def collect_blinds(self):
         small_blind_player = self.players[(self.dealer_position + 1) % len(self.players)]
