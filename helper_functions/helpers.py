c7777f23a27753627ce8d3967c85b73ab52a5ae6
Vincent Spinosa <vincentspinosa@gmail.com>
2023-05-27 14:37:47 +0200

    cpu_minus_one() deleted

--- helper_functions/helpers.py
+++ helper_functions/helpers.py
@@ -1,13 +1,5 @@
-import multiprocessing
-
 def force_int_input(question: str) -> int:
     try:
         return int(input(question))
     except:
         return force_int_input(question)
-    
-def cpu_minus_one():
-    cores = multiprocessing.cpu_count()
-    if cores <= 2:
-        return 1
-    return cores - 1
