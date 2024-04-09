746c1260679086b445daa9a64e109a79ea394d1b
Vincent Spinosa <vincentspinosa@MBP-de-Vincent.lan>
2024-04-09 15:30:01 +0200

    resetting

--- bin/match.py
+++
@@ -1,9 +0,0 @@
-from ui import UI
-from eval import evalAgent
-from ai import ai
-from helper_functions import helpers
-
-class Match(UI):
-    def __init__(self, players, target_player_index, dealer_position=0, small_blind=10, big_blind=20, current_pot=0, current_stage='pre-flop'):
-        super().__init__(players, target_player_index, dealer_position, small_blind, big_blind, current_pot, current_stage)
-
--- card.py
+++
@@ -1,7 +0,0 @@
-class Card:
-    def __init__(self, rank, suit):
-        self.rank = rank
-        self.suit = suit
-
-    def __str__(self):
-        return f"{self.rank} of {str(self.suit).upper()}"
