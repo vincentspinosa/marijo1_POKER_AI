53f8bebbf97575c1a5197063285a8ae4d06c8862
Vincent Spinosa <vincentspinosa@MacBook-Pro-de-Vincent.local>
2024-04-16 15:08:56 +0200

    back at strongest point BUT with extract_strat_values instead of shave_values + new raise buckets kept + regrets for call raise all-in same formula which changes change depending if check is available or not

--- ai/ai.py
+++ ai/ai.py
@@ -1,7 +1,11 @@
 import pickle
 import random
+import numpy as np
 from treys import Card
 from .gameState.gameState import GameState
+
+def sig(x):
+    return 1 / (1 + np.exp(-x))
 
 def find_max_value(actions:list) -> float:
     maxV = 0
@@ -10,23 +14,13 @@ def find_max_value(actions:list) -> float:
             maxV = ac[1]
     return maxV
 
-def turn_regrets_to_values(actions:list) -> list:
+def turn_action_regrets_to_values(actions:list) -> list:
     maxV = find_max_value(actions)
     for ac in actions:
         if ac[1] < 1:
             ac[1] = maxV
         else:
             ac[1] = (maxV / ac[1])
-    return actions
-
-def filter_actions(actions:list, winsCoefficient:float) -> list:
-    maxV = find_max_value(actions)
-    floor = maxV / (1 / winsCoefficient)
-    for ac in actions:
-        if ac[1] < floor:
-            ac[1] = 0
-        else:
-            ac[1] -= floor
     return actions
 
 def extract_strategy_values(actions:list) -> list:
@@ -36,7 +30,7 @@ def extract_strategy_values(actions:list) -> list:
             ac[1] *= (ac[1] / maxV)
     return actions
 
-def compute_distribution(actions:list) -> list:
+def compute_probabilities(actions:list) -> list:
     sum = 0
     for ac in actions:
         sum += ac[1]
@@ -45,17 +39,13 @@ def compute_distribution(actions:list) -> list:
             ac[1] /= sum
     return actions
 
-def compute_action_distribution(actions:list, winsCoefficient:float) -> list:
-    actions = turn_regrets_to_values(actions)
-    actions = filter_actions(actions, winsCoefficient)
-    for _ in range(4):
-        actions = extract_strategy_values(actions)
-    return compute_distribution(actions)
+def compute_regrets_probabilities(actions:list) -> list:
+    return compute_probabilities(extract_strategy_values(turn_action_regrets_to_values(actions)))
 
 def algorithm(gameState:GameState, iterations:int, verboseLevel:int=0, verboseIterationsSteps:int=50) -> dict[list, int]:
     # SETTING-UP EVERYTHING
-    actions_list = gameState.available_actions()
-    regrets = [[el, 0] for el in actions_list]
+    liste_actions = gameState.available_actions()
+    regrets = [[el, 0] for el in liste_actions]
     aiIndex = gameState.get_player_position(gameState.ai_player)
     opposite_player_index = (aiIndex + 1) % len(gameState.players)
     gameState.ai_deck = gameState.deck.cards + gameState.players[opposite_player_index].hand
@@ -80,7 +70,7 @@ def algorithm(gameState:GameState, iterations:int, verboseLevel:int=0, verboseIt
     loses = 0.01
     cc_to_deal = 5 - len(gameStateTemp.community_cards)
     # TRAVERSAL OF THE GAME TREE
-    for iter in range(30000):
+    for iter in range(iterations):
         games += 1
         sampleList = random.sample(gameStateTemp.ai_deck, cc_to_deal + 2)
         gameStateTemp.community_cards = gameState.community_cards + sampleList[:cc_to_deal]
@@ -93,7 +83,7 @@ def algorithm(gameState:GameState, iterations:int, verboseLevel:int=0, verboseIt
         else:
             loses += 1
         # VERBOSE
-        if verboseLevel > 1 and iter % verboseIterationsSteps == 0:
+        if verboseLevel > 2 and iter % verboseIterationsSteps == 0:
             print(f"\nIteration {iter}")
             print(f"Community cards:")
             Card.print_pretty_cards(gameStateTemp.community_cards)
@@ -101,9 +91,9 @@ def algorithm(gameState:GameState, iterations:int, verboseLevel:int=0, verboseIt
             Card.print_pretty_cards(gameStateTemp.players[opposite_player_index].hand)
     # COMPUTATION OF THE REGRETS
     winsCoefficient = wins / games
-    vA = 2
     index = -1
-    for action in actions_list:
+    uncertaintyValue = 8.34
+    for action in liste_actions:
         index += 1
         if action[0] == 'fold':
             if wins > 0.01:
@@ -112,16 +102,27 @@ def algorithm(gameState:GameState, iterations:int, verboseLevel:int=0, verboseIt
                 regrets[index][1] += ((potMinusDiff / 2) * draws)
         if action[0] == 'check':
             if wins > 0.01:
-                regrets[index][1] += ((potMinusDiff / 2) + ((maxBetAmount / vA) * winsCoefficient) * wins)
+                regrets[index][1] += ((potMinusDiff / 2) + ((maxBetAmount / (uncertaintyValue ** 2)) * winsCoefficient) * wins)
         if action[0] in ['call', 'raise', 'all-in']:
             if loses > 0.01:
-                regrets[index][1] += ((min(action[1], maxBetAmount) / winsCoefficient) * loses)
-            if wins > 0.01 and action[1] < maxBetAmount:
-                regrets[index][1] += ((((maxBetAmount - action[1]) / vA) * winsCoefficient) * wins)
-    # COMPUTATION OF THE RESULTS
-    result = compute_action_distribution(regrets, winsCoefficient)
+                if liste_actions[0][0] == 'check':
+                    regrets[index][1] += ((min(action[1], maxBetAmount) / winsCoefficient) * loses)
+                else:
+                    regrets[index][1] += (((min(action[1], maxBetAmount) * (1 + sig(uncertaintyValue))) / winsCoefficient) * loses)
+            if wins > 0.01:
+                if action[1] < maxBetAmount:
+                    regrets[index][1] += ((((maxBetAmount - action[1]) / (uncertaintyValue ** 2)) * winsCoefficient) * wins)
     # VERBOSE
     if verboseLevel > 0:
+        print(f"\nIterations: {iterations}")
+    if verboseLevel > 1:
+        print("\nRegrets before computing them:")
+        for r in regrets:
+            print(r)
+    # COMPUTATION OF THE RESULTS
+    result = compute_regrets_probabilities(regrets)
+    # VERBOSE
+    if verboseLevel > 1:
         print("\nAction distribution:")
         for action_distribution in result:
             print(action_distribution)
